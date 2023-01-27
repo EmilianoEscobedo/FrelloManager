f93dc0c7d939256cc273ec338d3622bf24c1a01f
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-01-27 14:49:18 -0300

    [Structure] mvn pkgs

---
+++ src/main/java/com/laingard/FrelloManager/FrelloManagerApplication.java
@@ -0,0 +1,13 @@
+package com.laingard.FrelloManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrelloManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrelloManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laingard/FrelloManager/FrelloManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laingard.FrelloManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrelloManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
