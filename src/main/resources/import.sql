70232114ab9249ad37bcf6109ae61f83d787a5dd
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-02 21:13:46 -0300

    [Test] Added sql import to create roles an admin user

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_SALES');
+INSERT INTO roles (name) VALUES ('ROLE_KITCHEN');
+INSERT INTO roles (name) VALUES ('ROLE_DELIVERY');
+INSERT INTO users (password, username, role_id) VALUES ('$2a$10$0lLmImRyzr/IKHQY8WDO/eX0A7B9bCNLN7DDAdJxLdiXKIYvXE7Ai', 'emi', '1');
