18998d9d139ec6cda62814fb5bc41bb2ae9d49ef
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-03 21:40:36 -0300

    [Structure] Enumeration folder

--- src/main/java/com/laingard/FrelloManager/repository/RoleRepository.java
+++ src/main/java/com/laingard/FrelloManager/repository/RoleRepository.java
@@ -1,6 +1,6 @@
 package com.laingard.FrelloManager.repository;
 
-import com.laingard.FrelloManager.model.ERole;
+import com.laingard.FrelloManager.enumeration.ERole;
 import com.laingard.FrelloManager.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
