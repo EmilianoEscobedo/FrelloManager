3e82e28fd875df7dd1820c628e66b22d416d326b
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-03 21:37:11 -0300

    [Structure] Folder impl

--- src/main/java/com/laingard/FrelloManager/service/Impl/RoleServiceImpl.java
+++ src/main/java/com/laingard/FrelloManager/service/Impl/RoleServiceImpl.java
@@ -1,18 +1,16 @@
-package com.laingard.FrelloManager.services;
+package com.laingard.FrelloManager.service.Impl;
 
-import com.laingard.FrelloManager.dto.UserDto;
+import com.laingard.FrelloManager.dto.RoleDto;
 import com.laingard.FrelloManager.exception.RoleNotFoundException;
 import com.laingard.FrelloManager.exception.UsernameNotFoundException;
-import com.laingard.FrelloManager.model.ERole;
+import com.laingard.FrelloManager.enumeration.ERole;
 import com.laingard.FrelloManager.model.Role;
 import com.laingard.FrelloManager.model.User;
 import com.laingard.FrelloManager.repository.RoleRepository;
 import com.laingard.FrelloManager.repository.UserRepository;
+import com.laingard.FrelloManager.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service(value = "roleService")
 public class RoleServiceImpl implements RoleService {
@@ -22,7 +20,7 @@ public class RoleServiceImpl implements RoleService {
     private RoleRepository roleRepository;
 
     @Override
-    public User assignRole(UserDto request) {
+    public User changeRole(RoleDto request) {
         if (userRepository.findOneByUsername(request.getUsername()).isEmpty()) {
             throw new UsernameNotFoundException("Error: Username not found");
         }
@@ -30,26 +28,26 @@ public class RoleServiceImpl implements RoleService {
         switch (request.getRole()) {
             case "user" -> {
                 Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
                 user.setRole(userRole);
             }
             case "sales" -> {
                 Role salesRole = roleRepository.findByName(ERole.ROLE_SALES)
-                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
                 user.setRole(salesRole);
             }
             case "kitchen" -> {
                 Role kitchenRole = roleRepository.findByName(ERole.ROLE_KITCHEN)
-                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
                 user.setRole(kitchenRole);
             }
             case "delivery" -> {
                 Role deliveryRole = roleRepository.findByName(ERole.ROLE_DELIVERY)
-                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
                 user.setRole(deliveryRole);
             }
             default -> {
-                throw new RoleNotFoundException("Error: Role is not found");
+                throw new RoleNotFoundException("Error: Role not found");
             }
         }
         return userRepository.save(user);
--- src/main/java/com/laingard/FrelloManager/service/Impl/UserServiceImpl.java
+++ src/main/java/com/laingard/FrelloManager/service/Impl/UserServiceImpl.java
@@ -1,19 +1,24 @@
-package com.laingard.FrelloManager.services;
+package com.laingard.FrelloManager.service.Impl;
 
 import com.laingard.FrelloManager.dto.UserDto;
 import com.laingard.FrelloManager.exception.UsernameAlreadyExistsException;
 import com.laingard.FrelloManager.exception.UsernameCantBeEmptyException;
-import com.laingard.FrelloManager.model.ERole;
+import com.laingard.FrelloManager.exception.UsernameNotFoundException;
+import com.laingard.FrelloManager.enumeration.ERole;
 import com.laingard.FrelloManager.model.Role;
 import com.laingard.FrelloManager.model.User;
 import com.laingard.FrelloManager.repository.RoleRepository;
 import com.laingard.FrelloManager.repository.UserRepository;
+import com.laingard.FrelloManager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service(value = "signupService")
-public class SignupServiceImpl implements SignupService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
@@ -24,6 +29,10 @@ public class SignupServiceImpl implements SignupService {
     @Autowired
     PasswordEncoder encoder;
 
+    public UserServiceImpl() {
+        super();
+    }
+
     @Override
     public User save(UserDto user) {
         if (userRepository.existsByUsername(user.getUsername())) {
@@ -32,12 +41,35 @@ public class SignupServiceImpl implements SignupService {
         if (user.getUsername() == null || user.getUsername().equals("")) {
             throw new UsernameCantBeEmptyException("Error: Username cant be empty");
         }
-        // Create new user's account
         User nUser = new User(user.getUsername(),
                 encoder.encode(user.getPassword()));
         Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
         nUser.setRole(userRole);
         return userRepository.save(nUser);
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        userRepository.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public User findOne(String username) {
+        if (userRepository.findOneByUsername(username).isEmpty()) {
+            throw new UsernameNotFoundException("Error: Username not found");
+        }
+        return userRepository.findOneByUsername(username).get();
+    }
+
+    @Override
+    public void deleteOne(String username){
+        if (userRepository.findOneByUsername(username).isEmpty()) {
+            throw new UsernameNotFoundException("Error: Username not found");
+        }
+        User user = userRepository.findOneByUsername(username).get();
+        userRepository.deleteById(user.getId());
     }
 }
