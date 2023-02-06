01287ad7e6f2f73f2735f0734400eec090835690
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-05 23:12:16 -0300

    [Structure] roleService to userService

--- src/main/java/com/laingard/FrelloManager/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package com.laingard.FrelloManager.service;
-
-import com.laingard.FrelloManager.dto.RoleDto;
-import com.laingard.FrelloManager.model.User;
-
-public interface RoleService {
-    User changeRole(RoleDto request);
-}
