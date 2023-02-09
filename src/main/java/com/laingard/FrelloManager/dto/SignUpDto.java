09c8e092d20b46c8aa86783aae5669c0aef7b7cd
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:45:13 -0300

    [Feature] Lombok impl

---
+++ src/main/java/com/laingard/FrelloManager/dto/SignUpDto.java
@@ -0,0 +1,11 @@
+package com.laingard.FrelloManager.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpDto {
+    private String username;
+    private String password;
+}
