01d474fd616e3e6e9ba1efba8da254b392499f37
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:35:24 -0300

    [Refactor] Lombok impl

--- src/main/java/com/laingard/FrelloManager/model/Product.java
+++ src/main/java/com/laingard/FrelloManager/model/Product.java
@@ -2,57 +2,30 @@ package com.laingard.FrelloManager.model;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "Stock")
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long Id;
-
-    @NotBlank
-    @Size(max = 30)
+    Long id;
     private String name;
     private Double quantity;
-    private Integer price;
+    private Double price;
 
     public Product() {
     }
 
-    public Product(String name, Double quantity, Integer price) {
+    public Product(String name, Double quantity, Double price) {
         this.name = name;
         this.quantity = quantity;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
         this.price = price;
     }
 }
--- src/main/java/com/laingard/FrelloManager/model/User.java
+++ src/main/java/com/laingard/FrelloManager/model/User.java
@@ -3,7 +3,11 @@ package com.laingard.FrelloManager.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users",
 uniqueConstraints = {
@@ -33,34 +37,5 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
     }
 }
