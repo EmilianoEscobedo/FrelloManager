58aad700d6a8601d069c7b6a866392df3dd09887
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:39:08 -0300

    [Refactor] Fixed imports

--- src/main/java/com/laingard/FrelloManager/security/jwt/AuthCredentials.java
+++ src/main/java/com/laingard/FrelloManager/security/jwt/AuthCredentials.java
@@ -1,9 +1,6 @@
 package com.laingard.FrelloManager.security.jwt;
 
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 
 @Data
 public class AuthCredentials {
