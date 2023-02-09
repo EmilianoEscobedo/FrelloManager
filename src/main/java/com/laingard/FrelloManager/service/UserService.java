8a6a491293562a3fdc1414a02c879c855556bbcc
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:48:12 -0300

    [Refactor] Changed dto types

--- src/main/java/com/laingard/FrelloManager/service/UserService.java
+++ src/main/java/com/laingard/FrelloManager/service/UserService.java
@@ -1,13 +1,14 @@
 package com.laingard.FrelloManager.service;
 
 import com.laingard.FrelloManager.dto.RoleDto;
+import com.laingard.FrelloManager.dto.SignUpDto;
 import com.laingard.FrelloManager.dto.UserDto;
 import com.laingard.FrelloManager.model.User;
 
 import java.util.List;
 
 public interface UserService {
-    User save(UserDto user);
+    User save(SignUpDto user);
     List<UserDto> findAll();
     UserDto findOne(Long id);
     void deleteOne(Long id);
