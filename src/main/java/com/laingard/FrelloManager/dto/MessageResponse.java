b7d5a49a80b51da4d82dff6bbfc81fcc9ed04f18
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:38:40 -0300

    [Feature] Lombok impl

--- src/main/java/com/laingard/FrelloManager/dto/MessageResponse.java
+++ src/main/java/com/laingard/FrelloManager/dto/MessageResponse.java
@@ -1,15 +1,14 @@
 package com.laingard.FrelloManager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MessageResponse {
     private String message;
 
     public MessageResponse(String message) {
-        this.message = message;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
         this.message = message;
     }
 }
