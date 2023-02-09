937b169f1906c3cbce9dd74176bc6ed33096532b
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:37:11 -0300

    [Feature] ProductOrderList rel and TimeStamp format

--- src/main/java/com/laingard/FrelloManager/model/Order.java
+++ src/main/java/com/laingard/FrelloManager/model/Order.java
@@ -4,33 +4,42 @@ import com.laingard.FrelloManager.enumeration.EState;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-
+@Getter
+@Setter
 @Entity
 @Table(name = "Orders")
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long Id;
-    @NotBlank
-    @Size(max = 30)
-    private List<Integer> products;
-    private final LocalDate timeStamp = LocalDate.now();
-    @NotBlank
+    Long id;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL,  orphanRemoval = true)
+    private List<OrderProduct> products;
+    private String timeStamp;
     private Double totalPrice;
-    @NotBlank
-    private EState state;
-    @NotBlank
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "state_id")
+    @Enumerated(EnumType.STRING)
+    private State state;
     private String clientName;
-    private Integer clientPhone;
-    @NotBlank
+    private String clientPhone;
     private String clientAddress;
 
-
-
+    @PrePersist
+    protected void onCreate() {
+        this.timeStamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
 
 
 
