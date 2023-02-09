93af7b34a5ea1ea46618ee8b59d6a7a1c1f50a72
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:46:36 -0300

    [Feature] Lombok impl

--- src/main/java/com/laingard/FrelloManager/dto/UserDto.java
+++ src/main/java/com/laingard/FrelloManager/dto/UserDto.java
@@ -1,34 +1,13 @@
 package com.laingard.FrelloManager.dto;
 
-import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 public class UserDto {
+    private Long id;
     private String username;
-    private String password;
     private String role;
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
