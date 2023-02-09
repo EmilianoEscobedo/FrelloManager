69910611821cbb5176365be632f78b4192429c0e
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:44:46 -0300

    [Feature] Lombok impl

--- src/main/java/com/laingard/FrelloManager/model/Role.java
+++ src/main/java/com/laingard/FrelloManager/model/Role.java
@@ -2,7 +2,11 @@ package com.laingard.FrelloManager.model;
 
 import com.laingard.FrelloManager.enumeration.ERole;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -19,22 +23,6 @@ public class Role {
     }
 
     public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
         this.name = name;
     }
 }
