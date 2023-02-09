97aef1dacb082baa8df8bd4cb9fec68ec261cb75
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-09 19:41:43 -0300

    [Feature] Built OrderProduct list persistance and DTO

---
+++ src/main/java/com/laingard/FrelloManager/dto/OrderProductDto.java
@@ -0,0 +1,11 @@
+package com.laingard.FrelloManager.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderProductDto {
+    private Long id;
+    private Double Quantity;
+}
---
+++ src/main/java/com/laingard/FrelloManager/model/OrderProduct.java
@@ -0,0 +1,33 @@
+package com.laingard.FrelloManager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Order_product")
+@NoArgsConstructor
+public class OrderProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Double quantity;
+
+    public OrderProduct(Order order, Product product, double quantity) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+    }
+}
