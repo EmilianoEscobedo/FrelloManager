085f139891b19bb06416cb4fa4343ffd69551975
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-12 20:45:17 -0300

    [Structure] Deleted unused dto and redundant entity

--- src/main/java/com/laingard/FrelloManager/repository/SalesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laingard.FrelloManager.repository;
-
-import com.laingard.FrelloManager.model.Sales;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalesRepository extends JpaRepository<Sales, Long> {
-}
