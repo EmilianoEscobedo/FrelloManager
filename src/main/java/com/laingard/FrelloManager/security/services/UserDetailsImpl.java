1c2648d1fdfafaaf168390e55c60df088ee64241
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-12 20:54:19 -0300

    [Refactor] Typo fixes

--- src/main/java/com/laingard/FrelloManager/security/services/UserDetailsImpl.java
+++ src/main/java/com/laingard/FrelloManager/security/services/UserDetailsImpl.java
@@ -5,12 +5,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
-public class UserDetailsImpl implements UserDetails {
+
+public class UserDetailsImpl implements UserDetails{
 
     private final User user;
     @Override
