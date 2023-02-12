4d417abcadc3dfd4fe44716c4f8a5694eb5df230
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-12 20:55:24 -0300

    [Feature] Added SQL order states to db init

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,4 +3,11 @@ INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_SALES');
 INSERT INTO roles (name) VALUES ('ROLE_KITCHEN');
 INSERT INTO roles (name) VALUES ('ROLE_DELIVERY');
+
+INSERT INTO states (name) VALUES ('COOKING');
+INSERT INTO states (name) VALUES ('DELIVERY');
+INSERT INTO states (name) VALUES ('DELIVERED');
+INSERT INTO states (name) VALUES ('CANCELED');
+INSERT INTO states (name) VALUES ('PAYED');
+
 INSERT INTO users (password, username, role_id) VALUES ('$2a$10$0lLmImRyzr/IKHQY8WDO/eX0A7B9bCNLN7DDAdJxLdiXKIYvXE7Ai', 'emi', '1');
