7ad982a6c88b53dddf2213b0a853fcf1ede37034
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-12 20:49:46 -0300

    [Refactor] Hashmap refactor updateRole method and dto returns

--- src/main/java/com/laingard/FrelloManager/service/Impl/UserServiceImpl.java
+++ src/main/java/com/laingard/FrelloManager/service/Impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.laingard.FrelloManager.service.Impl;
 
-import com.laingard.FrelloManager.dto.RoleDto;
 import com.laingard.FrelloManager.dto.SignUpDto;
 import com.laingard.FrelloManager.dto.UserDto;
 import com.laingard.FrelloManager.exception.AlreadyExistsException;
@@ -20,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserService {
@@ -36,7 +36,7 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     @Override
-    public User save(SignUpDto user) {
+    public UserDto save(SignUpDto user) {
         if (userRepository.existsByUsername(user.getUsername().toLowerCase())) {
             throw new AlreadyExistsException("Error: Username already exist");
         }
@@ -48,38 +48,29 @@ public class UserServiceImpl implements UserService {
         Role userRole = roleRepository.findByName(ERole.ROLE_USER)
                 .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
         nUser.setRole(userRole);
-        return userRepository.save(nUser);
+
+        userRepository.save(nUser);
+        return userMapper.toDto(nUser);
     }
     @Override
-    public User updateRole(RoleDto request, Long id) {
+    public UserDto updateRole(String RequestRole, Long id) {
         User user = userRepository.findOneById(id).
                 orElseThrow(()-> new NotFoundException("Error: User not found"));
-        switch (request.getRole()) {
-            case "user" -> {
-                Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
-                user.setRole(userRole);
-            }
-            case "sales" -> {
-                Role salesRole = roleRepository.findByName(ERole.ROLE_SALES)
-                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
-                user.setRole(salesRole);
-            }
-            case "kitchen" -> {
-                Role kitchenRole = roleRepository.findByName(ERole.ROLE_KITCHEN)
-                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
-                user.setRole(kitchenRole);
-            }
-            case "delivery" -> {
-                Role deliveryRole = roleRepository.findByName(ERole.ROLE_DELIVERY)
-                        .orElseThrow(() -> new RuntimeException("Error: Role not found."));
-                user.setRole(deliveryRole);
-            }
-            default -> {
-                throw new NotFoundException("Error: Role not found");
-            }
+        Map<String, ERole> roleMap = Map.of(
+        "user", ERole.ROLE_USER,
+        "sales", ERole.ROLE_SALES,
+        "kitchen", ERole.ROLE_KITCHEN,
+        "delivery", ERole.ROLE_DELIVERY);
+        ERole roleName = roleMap.get(RequestRole);
+        if (roleName == null) {
+            throw new NotFoundException("Error: Role not found");
         }
-        return userRepository.save(user);
+        Role role = roleRepository.findByName(roleName)
+                .orElseThrow(() -> new RuntimeException("Error: Role not found."));
+        user.setRole(role);
+
+        userRepository.save(user);
+        return userMapper.toDto(user);
     }
 
     @Override
--- src/main/java/com/laingard/FrelloManager/service/UserService.java
+++ src/main/java/com/laingard/FrelloManager/service/UserService.java
@@ -1,6 +1,5 @@
 package com.laingard.FrelloManager.service;
 
-import com.laingard.FrelloManager.dto.RoleDto;
 import com.laingard.FrelloManager.dto.SignUpDto;
 import com.laingard.FrelloManager.dto.UserDto;
 import com.laingard.FrelloManager.model.User;
@@ -8,9 +7,9 @@ import com.laingard.FrelloManager.model.User;
 import java.util.List;
 
 public interface UserService {
-    User save(SignUpDto user);
+    UserDto save(SignUpDto user);
     List<UserDto> findAll();
     UserDto findOne(Long id);
     void deleteOne(Long id);
-    User updateRole(RoleDto request, Long id);
+    UserDto updateRole(String role, Long id);
 }
