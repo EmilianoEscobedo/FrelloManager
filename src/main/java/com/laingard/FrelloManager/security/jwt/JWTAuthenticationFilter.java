c0392b041421a757e7db171f58431602511e39b3
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-02-16 15:31:01 -0300

    [Feature] Created context.auth obj

--- src/main/java/com/laingard/FrelloManager/security/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/laingard/FrelloManager/security/jwt/JWTAuthenticationFilter.java
@@ -8,9 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -37,20 +40,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         );
         return getAuthenticationManager().authenticate(usernamePAT);
     }
-
     @Override
     protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
-        String token = TokenUtils.createToken(userDetails.getUsername(), userDetails.getAuthorities());
 
+        UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails.getUsername(),
+                userDetails.getPassword(),
+                userDetails.getAuthorities()
+        );
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+        String token = TokenUtils.createToken(userDetails.getUsername(), userDetails.getAuthorities());
         response.addHeader("Authorization", token);
         response.getWriter().flush();
 
         super.successfulAuthentication(request, response, chain, authResult);
     }
-
-
 }
